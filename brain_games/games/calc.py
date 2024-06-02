6815cc843b97e501aa6b2827aec5ff3bc84cd36f
VS <gpt271@gmail.com>
2024-06-02 17:08:30 +0300

    add

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,11 +1,6 @@
 import random
 
-
-def welcome_user():
-    print("Welcome to the Brain Games!")
-    name = input("May I have your name? ")
-    print(f"Hello, {name}!")
-    return name
+GAME_DESCRIPTION = "What is the result of the expression?"  # Описание игры
 
 
 def generate_question():
@@ -16,36 +11,3 @@ def generate_question():
     question = f"{num1} {operation} {num2}"
     correct_answer = str(eval(question))
     return question, correct_answer
-
-
-def launch_game(name):
-    print("What is the result of the expression?")
-
-    correct_answers_count = 0
-    rounds_to_win = 3
-
-    while correct_answers_count < rounds_to_win:
-        question, correct_answer = generate_question()
-        print(f"Question: {question}")
-        user_answer = input("Your answer: ")
-
-        if user_answer == correct_answer:
-            print("Correct!")
-            correct_answers_count += 1
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            break
-
-    if correct_answers_count == rounds_to_win:
-        print(f"Congratulations, {name}!")
-
-
-def main():
-    name = welcome_user()
-    launch_game(name)
-
-
-if __name__ == "__main__":
-    main()
