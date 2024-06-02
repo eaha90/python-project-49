9666209c704499210e6f70722ff481ffef33d0c9
VS <gpt271@gmail.com>
2024-06-02 17:41:28 +0300

    new

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,37 +1,12 @@
 #!/usr/bin/env python3
 
-import random
-
-print("Welcome to the Brain Games!")
-name = input("May I have your name? ")
-print(f"Hello, {name}!")
-print('Answer "yes" if the number is even, otherwise answer "no".')
+from brain_games.games import even
+from brain_games.scripts.game_engine import launch_game
 
 
 def main():
-    correct_answers = 0
-    while correct_answers < 3:
-        number = random.randint(1, 100)
-        print(f"Question: {number}")
-        answer = input("Your answer: ").lower()
 
-        if answer == "yes" and number % 2 == 0:
-            print("Correct!")
-            correct_answers += 1
-        elif answer == "no" and number % 2 != 0:
-            print("Correct!")
-            correct_answers += 1
-        else:
-            print(f"'{answer}' is wrong answer ;(.")
-            if number % 2 == 0:
-                print("Correct answer was 'yes'.")
-            else:
-                print("Correct answer was 'no'.")
-            print(f"Let's try again, {name}!")
-            break
-
-    if correct_answers == 3:
-        print(f"Congratulations, {name}!")
+    launch_game(even)
 
 
 if __name__ == "__main__":
