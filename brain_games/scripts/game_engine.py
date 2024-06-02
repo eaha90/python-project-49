5817b6f53372159ccf5b062aba2b067262b629de
VS <gpt271@gmail.com>
2024-06-02 15:25:56 +0300

    add

---
+++ brain_games/scripts/game_engine.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+
+def welcome_user():
+
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    return name
+
+
+def launch_game(game_module):
+
+    name = welcome_user()
+    print(game_module.GAME_DESCRIPTION)
+
+    correct_answers_count = 0
+    rounds_to_win = 3
+
+    while correct_answers_count < rounds_to_win:
+        question, correct_answer = game_module.generate_question()
+        print(f"Question: {question}")
+        user_answer = input("Your answer: ")
+
+        if user_answer == correct_answer:
+            print("Correct!")
+            correct_answers_count += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
+
+    if correct_answers_count == rounds_to_win:
+        print(f"Congratulations, {name}!")
