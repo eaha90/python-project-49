66af566b6f7335c12f4f85f4e8a971903f46ef38
VS <gpt271@gmail.com>
2024-06-03 20:30:24 +0300

    new

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,9 +2,8 @@ import random
 
 GAME_DESCRIPTION = "Answer 'yes' if the number is even, otherwise answer 'no'."  # Описание игры
 
-
 def generate_question():
     number = random.randint(1, 100)
-    question = str(number)
-    correct_answer = "yes" if number % 2 == 0 else "no"
-    return question, correct_answer
+    question = f"Question: {number}"
+    answer = 'yes' if number % 2 == 0 else 'no'
+    return question, answer
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,38 @@
-#!/usr/bin/env python3
+from brain_games.games import even # type: ignore
+from brain_games.scripts.game_engine import launch_game # type: ignore
 
-from brain_games.games import even
-from brain_games.scripts.game_engine import launch_game
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    return name
 
 
 def main():
-    launch_game(even)
+    name = welcome_user()
+    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
 
+    rounds_to_win = 3
+    correct_answers_count = 0
+
+    while correct_answers_count < rounds_to_win:
+        question, correct_answer = even.generate_question()
+        print(f"Question: {question}")
+
+        user_answer = input("Your answer: ")
+
+        if user_answer == correct_answer:
+            print("Correct!")
+            correct_answers_count += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
+
+    if correct_answers_count == rounds_to_win:
+        print(f"Congratulations, {name}!")
 
 if __name__ == "__main__":
     main()
