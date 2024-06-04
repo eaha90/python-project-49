b3cab2b6c051479835b594d58119f2c8b34d3562
VS <gpt271@gmail.com>
2024-06-04 21:53:38 +0300

    new

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,13 +1,28 @@
-import random
+from random import randint
 
-GAME_DESCRIPTION = "What is the result of the expression?"
+GAME_DESCRIPTION = 'What is the result of the expression?'
+
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    return name
 
 
 def generate_question():
-    operations = ['+', '-', '*']
-    num1 = random.randint(1, 50)
-    num2 = random.randint(1, 20)
-    operation = random.choice(operations)
-    question = f"{num1} {operation} {num2}"
-    correct_answer = str(eval(question))
-    return question, correct_answer
+    operand1 = randint(1, 100)
+    operand2 = randint(1, 100)
+    operator = randint(1, 3)
+
+    if operator == 1:
+        expression = f"{operand1} + {operand2}"
+        correct_answer = str(operand1 + operand2)
+    elif operator == 2:
+        expression = f"{operand1} - {operand2}"
+        correct_answer = str(operand1 - operand2)
+    else:
+        expression = f"{operand1} * {operand2}"
+        correct_answer = str(operand1 * operand2)
+
+    return expression, correct_answer
