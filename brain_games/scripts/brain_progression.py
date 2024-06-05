c66ab384b395faf889238ca18cc19abf010a98d6
VS <gpt271@gmail.com>
2024-06-05 22:04:30 +0300

    add

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -6,7 +6,10 @@ from brain_games.scripts.game_engine import launch_game
 
 def main():
     print(progression.GAME_DESCRIPTION)
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+
     launch_game(progression)
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
