292487a1a57b2a1cbcb57f8d0dc726518615e7a4
VS <gpt271@gmail.com>
2024-06-05 21:23:17 +0300

    add

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,25 +1,11 @@
-# brain_games/games/gcd.py
+# brain_games/scripts/brain_gcd.py
 
-from random import randint
-
-GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.'
-
-
-def welcome_user():
-
-    print("Welcome to the Brain Games!")
-    name = input("May I have your name? ")
-    print(f"Hello, {name}!")
-    return name
+from brain_games.games import gcd
+from brain_games.scripts.game_engine import launch_game
 
 
-def generate_question():
+def main():
+    launch_game(gcd)
 
-    number1 = randint(1, 100)
-    number2 = randint(1, 100)
-    gcd = 1
-    for i in range(1, min(number1, number2) + 1):
-        if number1 % i == 0 and number2 % i == 0:
-            gcd = i
-
-    return f"{number1} {number2}", str(gcd)
+if __name__ == "__main__":
+    main()
