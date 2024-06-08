d9ad8e8e16119f7baabbbf065056e9ea6a725d11
VS <gpt271@gmail.com>
2024-06-08 13:48:42 +0300

    add

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,23 +1,20 @@
-from random import randint
+import random
 
 GAME_DESCRIPTION = 'What number is missing in this progression?'
 
 
-def welcome_user():
-
-    print("Welcome to the Brain Games!")
-    name = input("May I have your name? ")
-    print(f"Hello, {name}!")
-    return name
-
-
 def generate_question():
 
-    start = randint(1, 10)
-    step = randint(1, 10)
-    progression_length = randint(5, 10)
-    hidden_index = randint(0, progression_length - 1)
+    start = random.randint(1, 10)
+    step = random.randint(1, 10)
+    progression_length = random.randint(5, 10)
     progression = [start + i * step for i in range(progression_length)]
-    progression[hidden_index] = ".."
-    correct_answer = str(start + hidden_index * step)
-    return " ".join(str(x) for x in progression), correct_answer
+    hidden_index = random.randint(0, progression_length - 1)
+    correct_answer = str(progression[hidden_index])
+    progression[hidden_index] = '..'
+    question = ' '.join(str(item) for item in progression)
+    return question, correct_answer
+
+
+def get_user_answer():
+    return input("Your answer: ")
--- brain_games/scripts/game_engine.py
+++ brain_games/scripts/game_engine.py
@@ -1,25 +1,23 @@
+import prompt
+
+
 def launch_game(game_module):
-    name = game_module.welcome_user()
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
     print(game_module.GAME_DESCRIPTION)
+    question, correct_answer = game_module.generate_question()
+    print(f"Question: {question}")
 
-    correct_answers_count = 0
-    rounds_to_win = 3
+    if hasattr(game_module, 'get_user_answer'):
+        user_answer = game_module.get_user_answer()
+    else:
+        user_answer = prompt.string("Your answer: ")
 
-    while correct_answers_count < rounds_to_win:
-        question, correct_answer = game_module.generate_question()
-        print(f"Question: {question}")
-
-        user_answer = input("Your answer: ")
-
-        if user_answer == correct_answer:
-            print("Correct!")
-            correct_answers_count += 1
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            break
-
-    if correct_answers_count == rounds_to_win:
-        print(f"Congratulations, {name}!")
-
+    if user_answer == correct_answer:
+        print("Correct!")
+    else:
+        print(
+            f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'"
+        )
+        print(f"Let's try again, {name}!")
