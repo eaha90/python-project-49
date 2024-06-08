2606515cdef3a570cf2cc072e0e31f0efeb4c0bb
VS <gpt271@gmail.com>
2024-06-08 14:15:37 +0300

    add

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,7 +6,7 @@ GAME_DESCRIPTION = 'What number is missing in this progression?'
 def generate_question():
     start = random.randint(1, 10)
     step = random.randint(1, 10)
-    progression_length = random.randint(5, 10)
+    progression_length = random.randint(5, 11)
     progression = [start + i * step for i in range(progression_length)]
     hidden_index = random.randint(0, progression_length - 1)
     correct_answer = str(progression[hidden_index])
