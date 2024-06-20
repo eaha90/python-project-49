6efc57e36b76cfc09a148f79344b68dfc336ea7c
VS <gpt271@gmail.com>
2024-06-20 21:39:32 +0300

    add

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,6 +1,5 @@
 import random
 
-
 MIN_NUMBER = 1
 MAX_NUMBER = 10
 
@@ -9,6 +8,13 @@ def game_calc():
     random_int_1 = random.randint(MIN_NUMBER, MAX_NUMBER)
     random_int_2 = random.randint(MIN_NUMBER, MAX_NUMBER)
     action = random.choice(['*', '+', '-'])
-    operation = ' '.join((str(random_int_1), action, str(random_int_2)))
-    result = int(eval(operation))
+    
+    if action == '+':
+        result = random_int_1 + random_int_2
+    elif action == '-':
+        result = random_int_1 - random_int_2
+    elif action == '*':
+        result = random_int_1 * random_int_2
+    
+    operation = f'{random_int_1} {action} {random_int_2}'
     return operation, result
