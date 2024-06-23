c03c1927f990e33c51e2f6efcf251d059ec877b4
VS <gpt271@gmail.com>
2024-06-23 21:32:05 +0300

    add

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.engine import run_game, QUESTION
-from brain_games.games.game_calc import game_calc
+from brain_games.games.game_calc import game_calc, QUESTION
+from brain_games.engine import run_game
 
 
 def main():
     run_game(game_calc, QUESTION)
-
 
 if __name__ == '__main__':
     main()
