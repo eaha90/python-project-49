5deda55fc3daeca730051e7ed08c7e5641120e3f
VS <gpt271@gmail.com>
2024-06-24 21:14:15 +0300

    add

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -10,13 +10,13 @@ def game_calc():
     random_int_1 = random.randint(MIN_NUMBER, MAX_NUMBER)
     random_int_2 = random.randint(MIN_NUMBER, MAX_NUMBER)
     action = random.choice(['*', '+', '-'])
-    
+
     if action == '+':
         result = random_int_1 + random_int_2
     elif action == '-':
         result = random_int_1 - random_int_2
     elif action == '*':
         result = random_int_1 * random_int_2
-    
+
     operation = f'{random_int_1} {action} {random_int_2}'
     return operation, result
