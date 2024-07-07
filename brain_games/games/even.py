44c42a5988dea6d2180569a6344a9db0323f7d7e
VS <gpt271@gmail.com>
2024-07-07 17:21:39 +0300

    add

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,27 +1,14 @@
 import random
 
-DESCRIPTION = 'Answer "yes" if given number is prime or even. Otherwise answer "no".'
 
-
-def is_prime(number):
-    if number <= 1:
-        return False
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
-            return False
-    return True
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(number):
-    return number % 2 == 0
+    return False if number % 2 else True
 
 
-def get_question_and_answer(check_type):
+def get_question_and_answer():
     number = random.randint(1, 100)
-    if check_type == 'prime':
-        answer = "yes" if is_prime(number) else "no"
-    elif check_type == 'even':
-        answer = "yes" if is_even(number) else "no"
-    else:
-        raise ValueError("Unknown check type: {}".format(check_type))
+    answer = "yes" if is_even(number) else "no"
     return str(number), answer
