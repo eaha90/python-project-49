45af566654bd5729069455867cb961f7138bfd26
VS <gpt271@gmail.com>
2024-07-07 17:40:46 +0300

    add

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,18 +1,21 @@
 import random
 
+
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-
-
-def is_prime(number):
-    if number <= 1:
-        return False
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
-            return False
-    return True
 
 
 def get_question_and_answer():
     number = random.randint(1, 100)
+
+
+    def is_prime(number):
+        if number == 1:
+            return False
+        root = int(number ** 0.5)
+        possible_deviders = list(range(2, root + 1))
+        for n in possible_deviders:
+            if number % n == 0:
+                return False
+        return True
     answer = "yes" if is_prime(number) else "no"
     return str(number), answer
